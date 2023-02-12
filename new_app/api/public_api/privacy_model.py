9db9aa0657cd972aceeb4fedcab711813dab4121
orib <oribrau@gmail.com>
2023-02-12 18:09:54 +0200

    add requirements and attach model to public api

--- new_app/api/public_api/privacy_model.py
+++ new_app/api/public_api/privacy_model.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, JsonResponse
 from .base_api import BasePublicApi
 from new_app.api.jsonResponse import baseHttpResponse
 
+from privacy_classifier.pipeline.pipeline import PrivacyClassifierPipeline
+pipeline = PrivacyClassifierPipeline()
 class PrivacyModelApi(BasePublicApi):
     # authentication_classes = [SessionAuthentication, BasicAuthentication]
     # permission_classes = [IsAuthenticated]
@@ -13,11 +15,11 @@ class PrivacyModelApi(BasePublicApi):
     def post(self, request, format=None):
         boolean = random.choice([True, False])
         prompt = request.data['prompt']
-
-
+        res = pipeline(text=prompt)
         return JsonResponse({
             'err': 0,
             'errMessage': '',
+            'model_res': res,
             'suggested_prompt': prompt + ' suggested',
             'suggested_model': ' Privacy model V1',
             'pass_privacy': False,
