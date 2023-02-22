0b69d16fd58bd985e03f412919fb59ebb1827d3b
orib <oribrau@gmail.com>
2023-02-22 11:18:33 +0200

    added another ip

--- deploy/nginx_conf.sh
+++ deploy/nginx_conf.sh
@@ -1,4 +1,5 @@
 sudo cp /home/ubuntu/app/gaia/deploy/gaia_nginx.conf /etc/nginx/sites-available/gaia_nginx.conf
 sudo ln -s /etc/nginx/sites-available/gaia_nginx.conf /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/app/gaia/deploy/gaia.ini /etc/uwsgi/sites/gaia.ini
 #sudo nano /etc/uwsgi/sites/gaia.ini
 #sudo nano /etc/systemd/system/uwsgi.service
