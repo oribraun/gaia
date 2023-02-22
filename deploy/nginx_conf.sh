7e1caf2ea0a640f646fdb3f6afbbd639d352d42b
orib <oribrau@gmail.com>
2023-02-22 09:26:01 +0200

    fix for uwsgi

--- deploy/nginx_conf.sh
+++ deploy/nginx_conf.sh
@@ -1,2 +1,4 @@
 sudo cp /home/ubuntu/app/gaia/deploy/gaia_nginx.conf /etc/nginx/sites-available/gaia_nginx.conf
 sudo ln -s /etc/nginx/sites-available/gaia_nginx.conf /etc/nginx/sites-enabled/
+#sudo nano /etc/uwsgi/sites/gaia.ini
+#sudo nano /etc/systemd/system/uwsgi.service
