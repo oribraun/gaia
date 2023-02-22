5924febfbf896f4ed7b168bf339de6f05a07e178
orib <oribrau@gmail.com>
2023-02-22 09:06:08 +0200

    update nginx_conf

--- deploy/nginx_conf.sh
+++ deploy/nginx_conf.sh
@@ -1,2 +1,2 @@
-sudo cp /home/ubuntu/app/gaia/deploy/gaia.conf /etc/nginx/sites-available/gaia.conf
-sudo ln -s /etc/nginx/sites-available/gaia.conf /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/app/gaia/deploy/gaia_nginx.conf /etc/nginx/sites-available/gaia_nginx.conf
+sudo ln -s /etc/nginx/sites-available/gaia_nginx.conf /etc/nginx/sites-enabled/
