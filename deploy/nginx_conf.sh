bc995c6de1a178be957cb65ad7995ae512f68e91
orib <oribrau@gmail.com>
2023-02-23 17:13:18 +0200

    added gaia https

--- deploy/nginx_conf.sh
+++ deploy/nginx_conf.sh
@@ -5,7 +5,7 @@ sudo systemctl restart nginx
 #sudo nano /etc/uwsgi/sites/gaia.ini
 #sudo nano /etc/systemd/system/uwsgi.service
 
-sudo cp /home/ubuntu/app/gaia/deploy/gaia_nginx.conf /etc/nginx/sites-available/gaia_nginx_https.conf
+sudo cp /home/ubuntu/app/gaia/deploy/gaia_nginx_https.conf /etc/nginx/sites-available/gaia_nginx_https.conf
 sudo ln -s /etc/nginx/sites-available/gaia_nginx_https.conf /etc/nginx/sites-enabled/
 sudo cp /home/ubuntu/app/gaia/deploy/gaia_https.ini /etc/uwsgi/sites/gaia_https.ini
 sudo systemctl restart nginx
--- deploy/run.sh
+++ deploy/run.sh
@@ -1,4 +1,4 @@
 uwsgi --socket /run/uwsgi/gaia.sock --chdir /home/ubuntu/app/gaia --module main_app.wsgi --chmod-socket=666
 
 # https
-uwsgi --socket /run/uwsgi/gaia_https.sock --chdir /home/ubuntu/app/gaia --module main_app.wsgi --chmod-socket=666
+uwsgi --socket /run/uwsgi/gaia_https.sock --chdir /home/ubuntu/app/gaia --module main_app.wsgi --chmod-socket=667
