6cd4059fa0e7df1ba9a6e134dd0a1efbe8bb343f
orib <oribrau@gmail.com>
2023-02-23 13:54:16 +0200

    test

--- deploy/nginx_conf.sh
+++ deploy/nginx_conf.sh
@@ -1,5 +1,6 @@
 sudo cp /home/ubuntu/app/gaia/deploy/gaia_nginx.conf /etc/nginx/sites-available/gaia_nginx.conf
 sudo ln -s /etc/nginx/sites-available/gaia_nginx.conf /etc/nginx/sites-enabled/
 sudo cp /home/ubuntu/app/gaia/deploy/gaia.ini /etc/uwsgi/sites/gaia.ini
+sudo systemctl restart nginx
 #sudo nano /etc/uwsgi/sites/gaia.ini
 #sudo nano /etc/systemd/system/uwsgi.service
