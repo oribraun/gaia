2d9879e5d8345937ad1b16e0548778bd2886c968
orib <oribrau@gmail.com>
2023-02-23 13:04:18 +0200

    test

--- main_app/settings.py
+++ main_app/settings.py
@@ -185,6 +185,7 @@ CORS_ORIGIN_WHITELIST = [
     'chrome-extension://ghkjadifhfhebgfbcmgoklkkhapjjmbj'
 ]
 CORS_ALLOWED_ORIGIN = [
+    'chrome-extension://ghkjadifhfhebgfbcmgoklkkhapjjmbj'
     # 'http://localhost:4200',
     # 'http://localhost:4201'
 ]
