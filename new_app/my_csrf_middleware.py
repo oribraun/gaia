6fdf1daba0ed1a04f76741511c55e0f350080dd5
orib <oribrau@gmail.com>
2023-02-24 10:24:25 +0200

    test

--- new_app/my_csrf_middleware.py
+++ new_app/my_csrf_middleware.py
@@ -4,7 +4,10 @@ class MyCsrfViewMiddleware(CsrfViewMiddleware):
     def process_view(self, request, callback, callback_args, callback_kwargs):
         print('MyCsrfViewMiddleware process_view request', request.COOKIES)
         csrf_secret = self._get_secret(request)
-        print('MyCsrfViewMiddleware process_view request', csrf_secret)
+        request_csrf_token = request.POST.get("csrfmiddlewaretoken", "")
+        print('MyCsrfViewMiddleware process_view request_csrf_token', request_csrf_token)
+        request_csrf_token = request.META
+        print('MyCsrfViewMiddleware process_view META', request_csrf_token)
         return super().process_view(request, callback, callback_args, callback_kwargs)
 
     def process_response(self, request, response):
