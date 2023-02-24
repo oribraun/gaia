ea983dfa901f07c270e121db9fad65094f104c64
orib <oribrau@gmail.com>
2023-02-24 09:18:50 +0200

    test

--- new_app/api/auth.py
+++ new_app/api/auth.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.response import Response
 from django.contrib.auth import login, logout
 from django.contrib.auth.tokens import default_token_generator
-from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.utils.decorators import method_decorator
 from django.urls import reverse
 from django.utils import timezone
@@ -48,10 +48,10 @@ class RegisterAPI(generics.GenericAPIView):
 # Login API
 
 class LoginAPI(generics.GenericAPIView):
-    # authentication_classes = [BasicAuthentication]
+    authentication_classes = [BasicAuthentication]
     serializer_class = LoginSerializer
 
-    @method_decorator(csrf_exempt, name='post')
+    @method_decorator(ensure_csrf_cookie, name='post')
     def post(self, request, *args, **kwargs):
         print('request', request)
         serializer = MyAuthTokenSerializer(data=request.data)
