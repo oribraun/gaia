76056a91c39ef01747969efaac33c8222b759917
orib <oribrau@gmail.com>
2023-02-24 10:32:23 +0200

    test

--- new_app/my_csrf_middleware.py
+++ new_app/my_csrf_middleware.py
@@ -1,4 +1,5 @@
 from django.middleware.csrf import CsrfViewMiddleware
+from django.conf import settings
 
 class MyCsrfViewMiddleware(CsrfViewMiddleware):
     def process_view(self, request, callback, callback_args, callback_kwargs):
@@ -6,10 +7,12 @@ class MyCsrfViewMiddleware(CsrfViewMiddleware):
         csrf_secret = self._get_secret(request)
         request_csrf_token = request.POST.get("csrfmiddlewaretoken", "")
         print('MyCsrfViewMiddleware process_view request_csrf_token', request_csrf_token)
-        request_csrf_token = request.META['HTTP_X_CSRFTOKEN']
-        print('MyCsrfViewMiddleware process_view csrf_secret HTTP_X_CSRFTOKEN', request_csrf_token)
-        request_csrf_token = request.META['CSRF_COOKIE']
-        print('MyCsrfViewMiddleware process_view csrf_secret CSRF_COOKIE', request_csrf_token)
+        CSRF_HEADER_NAME = settings.CSRF_HEADER_NAME
+        print('MyCsrfViewMiddleware process_view CSRF_HEADER_NAME', CSRF_HEADER_NAME)
+        # request_csrf_token = request.META['HTTP_X_CSRFTOKEN']
+        # print('MyCsrfViewMiddleware process_view csrf_secret HTTP_X_CSRFTOKEN', request_csrf_token)
+        # request_csrf_token = request.META['CSRF_COOKIE']
+        # print('MyCsrfViewMiddleware process_view csrf_secret CSRF_COOKIE', request_csrf_token)
         return super().process_view(request, callback, callback_args, callback_kwargs)
 
     def process_response(self, request, response):
