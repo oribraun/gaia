62403856c600a15c6c794c29069c1bcc59d4a4b6
orib <oribrau@gmail.com>
2023-02-24 10:17:43 +0200

    test

--- new_app/my_csrf_middleware.py
+++ new_app/my_csrf_middleware.py
@@ -2,9 +2,9 @@ from django.middleware.csrf import CsrfViewMiddleware
 
 class MyCsrfViewMiddleware(CsrfViewMiddleware):
     def process_view(self, request, callback, callback_args, callback_kwargs):
-        print('MyCsrfViewMiddleware request', request)
+        print('MyCsrfViewMiddleware process_view request', request)
         return super().process_view(request, callback, callback_args, callback_kwargs)
 
     def process_response(self, request, response):
-        print('MyCsrfViewMiddleware request', request)
-        return super().process_response(request, response)
+        print('MyCsrfViewMiddleware process_response request', request)
+        super().process_response(request, response)
