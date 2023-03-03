72a484cdf364d75529be07a01fa9b41c8e1c10b2
orib <oribrau@gmail.com>
2023-03-03 13:27:53 +0200

    sending email with threads

--- new_app/email_service.py
+++ new_app/email_service.py
@@ -4,6 +4,7 @@ from django.utils.encoding import force_bytes
 from django.urls import reverse
 from django.core.mail import send_mail
 from django.utils import timezone
+import threading
 
 class EmailService():
 
@@ -19,7 +20,7 @@ class EmailService():
         })
         verify_url = request.build_absolute_uri(verify_url)
 
-        EmailService.send_email(
+        EmailService.send_email_thread(
             user.email,
             'Gaia verification',
             f'Please follow this link to verify your email: {verify_url}',
@@ -40,18 +41,32 @@ class EmailService():
         user.password_reset_timestamp = timezone.now()
         user.save()
 
-        EmailService.send_email(
+        EmailService.send_email_thread(
             user.email,
             'Password Reset',
             f'Please follow this link to reset your password: {reset_url}',
         )
 
     @staticmethod
-    def send_email(emil, header, message):
+    def send_email(email, subject, message):
         send_mail(
-            header,
+            subject,
             message,
             'ori@gaialabs.ai',
-            [emil],
+            [email],
             fail_silently=False,
         )
+
+
+    @staticmethod
+    def send_email_thread(email, subject, message):
+        threading.Thread(
+
+            # call to send_html_mail
+            target=EmailService.send_email,
+
+            kwargs={
+                "email": email,
+                "subject": subject,
+                "message": message
+            }).start()
