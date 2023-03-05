42a8420d8b2a632cedae89b4042ced03e114b34e
orib <oribrau@gmail.com>
2023-03-05 18:25:52 +0200

    added email que added cron jobs

--- new_app/cron_jobs/send_emails.py
+++ new_app/cron_jobs/send_emails.py
@@ -10,6 +10,6 @@ def start():
             item.subject,
             item.message,
             item.sender,
-            json.loads(item.recipient_list),
+            json.load(item.recipient_list),
         )
         EmailService.set_sent(item, True)
