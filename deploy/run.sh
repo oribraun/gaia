350fe623595c129e0cd5bf48a2cfefae26c8a847
orib <oribrau@gmail.com>
2023-03-05 18:57:36 +0200

    added comments

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,4 +3,6 @@ uwsgi --socket /run/uwsgi/gaia.sock --chdir /home/ubuntu/app/gaia --module main_
 # https
 uwsgi --socket /run/uwsgi/gaia_https.sock --chdir /home/ubuntu/app/gaia --module main_app.wsgi --chmod-socket=667
 
-python manage.py crontab add
+#python manage.py crontab add
+#cronjob -e
+#/home/ubuntu/app/gaia/venv/bin/python /home/ubuntu/app/gaia/manage.py crontab run 73274fe689719bb6e19498816411605d
