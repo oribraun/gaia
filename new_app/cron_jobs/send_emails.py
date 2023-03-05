0aabf0b00453f295fc244938f9012c98e8517c6f
orib <oribrau@gmail.com>
2023-03-05 18:29:05 +0200

    added email que added cron jobs

--- new_app/cron_jobs/send_emails.py
+++ new_app/cron_jobs/send_emails.py
@@ -3,7 +3,6 @@ from new_app.app_models.email_que import EmailQue
 from new_app.email_service import EmailService
 
 def start():
-    print('started cron')
     list = EmailQue.objects.filter(sent=False)
     for item in list.iterator():
         EmailService.send_email(
